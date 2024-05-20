66287bf1b5a1e5a91b3db590fbbb2285b91432ff
LeminGhori <ghorilemin@gmail.com>
2024-05-20 12:54:54 +0530

    organization page created

--- src/page/Organization/Organization.jsx
+++ src/page/Organization/Organization.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
-function index() {
+import CorporateFareIcon from '@mui/icons-material/CorporateFare';
+import "./index.scss"
+function Organization() {
     return (
-        <div>index</div>
+        <div className='organization-contener'>
+            <div className='organization-title'>
+                <div className='organization-title-text'>Organizations</div>
+                <div className='organization-title-sub-text'>Pick the organization you want to access to</div>
+            </div>
+            <div className='organization-list'>
+                <div className='organization-content'>
+                    <div className='organization-icon'><CorporateFareIcon /></div>
+                    <div className='organization-name'>Organization Name A</div>
+                </div>
+                <div className='organization-content'>
+                    <div className='organization-icon'><CorporateFareIcon /></div>
+                    <div className='organization-name'>Organization Name A</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default index
+export default Organization
